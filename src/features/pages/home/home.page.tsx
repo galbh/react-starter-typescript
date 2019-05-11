cc15adacc603d55af02eaf4c5f4096ae8703d469
Gal Ben Haim <gal.benhaim@solaredge.com>
2019-05-12 02:17:55 +0300

    change home to tsx

--- src/features/pages/home/home.page.tsx
+++ src/features/pages/home/home.page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const HomePage = () => {
+const HomePage: React.FC<{}> = () => {
   const [t] = useTranslation();
   return <div>{t('HOME_PAGE')}</div>;
 };
--- src/features/pages/not-found/not-found.page.tsx
+++ src/features/pages/not-found/not-found.page.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { ROUTES } from '../../../common/constants';
 
-const PageNotFound = () => <Redirect to={ROUTES.home} />;
+const PageNotFound: React.FC<{}> = () => <Redirect to={ROUTES.home} />;
 
 export default PageNotFound;
